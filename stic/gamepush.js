ec3c44623e20522432ac3374ba9a487f9fa2d257
bobydob <80217817+bobydob@users.noreply.github.com>
2025-01-07 04:41:18 +0300

    Add files via upload

--- stic/gamepush.js
+++ stic/gamepush.js
@@ -9820,7 +9820,7 @@ console.error = function() {
         })
     }, a.f = {}, a.e = e => Promise.all(Object.keys(a.f).reduce(((t, n) => (a.f[n](e, t), t)), [])), a.u = e => (({
         72: "utils/centrifuge.protobuf",
-        369: "static",
+        369: "platforms/platform.gm",
         423: "i18n/external-sdk-tr-json",
         477: "platforms/platform.gp",
         848: "i18n/ja-json",
